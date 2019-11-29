71bd297f965a97d2b45be3a1b2c3bcc92139034a
rafael-buttignon <rafaaugustocontato@gmail.com>
2019-11-28 21:11:32 -0300

    primeiro commit

--- QuickBuy.Web/Startup.cs
+++ QuickBuy.Web/Startup.cs
@@ -56,8 +56,7 @@ namespace QuickBuy.Web
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
+   
 
                 spa.Options.SourcePath = "ClientApp";
 
